3e7b789b92b7217d70c345a26d83e188cdf18006
sakuramochi <sakuramochi.mochi.0@gmail.com>
2017-12-28 12:11:25 +0900

    一般公開用に説明文の追加や細かいレイアウトの調整。

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index),
+    url(r'^$', views.index, name='index'),
     url(r'^page/(\d+)/$', views.page, name='page'),
     url(r'^tag/([^/]+)/$', views.tag, name='tag'),
     url(r'^tag/([^/]+)/(\d+)/$', views.tag, name='tag'),
